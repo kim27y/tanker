883c293a0e0495a1b780ed080d66f11de60f87b3
kim27y <kim27y@naver.com>
2022-05-15 21:28:18 +0900

    tanker.py

--- dags/tanker.py
+++ dags/tanker.py
@@ -20,10 +20,11 @@ def take_data(ti): #확장성을 위해 type 별 json 전달하도록 만들기
     video_root = str(Variable.get('file_root')) # Variable로 할 지, 다른 방식으로 할지 설정
     if not video_root:
         raise Exception('No video root.')
-    video_root=video_root+'/video.mp4'#환경에 맞게 변경 (테스트용 경로)
+    mp4_list = [filename for filename in glob.iglob(os.path.join(video_root,'**/*'), recursive=True) if filename.lower().endswith('mp4')]#환경에 맞게 변경 (테스트용 경로)
+    video_root = mp4_list[0]
     return video_root
     
-def check_data(ti): #데이터의 
+def check_data(ti): #데이터 분류
     ti = str(ti.xcom_pull(task_ids=['take_data'])[0])
     task_id=''
     if os.path.splitext(ti)[1] == '.mp4':
@@ -36,16 +37,12 @@ def process_video(ti):
     ti = str(ti.xcom_pull(task_ids=['take_data'])[0]) #현재는 str, 나중에는 json으로 받아와서 데이터 종류별로 처리방식 다르게
     makedirs(ti[:-4])
     vidcap = cv2.VideoCapture(ti)
-
     count = 1
-
     while True:
         success,image = vidcap.read()
         if not success:
             break
         cv2.imwrite(f"{ti[:-4]}/{count}.jpg", image) # 이부분에 image를 데이터베이스에 넣도록 처리하면 됨 (비디오이름, 시간, 프레임번째수, 비디오)
-        
-    
         count += 1
 
 
@@ -77,7 +74,7 @@ with DAG(
         task_id='process_csv',
         python_callable=process_csv
     )
-    # 4. Save processed datetime # 시각화 부분으로 만들면 되지 않을까
+    # 4. Save processed datetime # 시각화 부분
     # task_save_video = PythonOperator(
     #     task_id='save_video',
     #     python_callable=save_video
